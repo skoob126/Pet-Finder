5eb8b0e927686698749175ee9a02a8e2160dd7dd
Chadi Bouaazzi <chadwickbouaazzi@gmail.com>
2020-06-03 09:40:35 -0500

    Added user id to members url

--- public/js/members.js
+++ public/js/members.js
@@ -3,5 +3,11 @@ $(document).ready(() => {
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
+    const userId = data.id;
+    const postButton = $("#newPost");
+
+    postButton.append(
+      "<a href='/newpost?User_id=" + userId + "'>Create a Post</a>"
+    );
   });
 });
