416356f24ccc0b1827373498fde513929b2da57c
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-08 18:00:00 -0500

    members.js organized and notes added

--- public/js/members.js
+++ public/js/members.js
@@ -1,5 +1,6 @@
 /* eslint-disable quotes */
 $(document).ready(() => {
+  // global variables defined
   const golbalContainer = $("#globalPost");
   const userContainer = $("#userPost");
   const postCategory = $("#category");
@@ -7,13 +8,12 @@ $(document).ready(() => {
   let userId;
   let newLocation = "All Locations";
   let posts;
-  // This file just does a GET request to figure out which user is logged in
-  // and updates the HTML on the page
+
+  // get request to figure out which user is logged in and updates the HTML on the page with user specific details
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
     userId = data.id;
     const postButton = $(".navbar-header");
-
     postButton.append(
       "<a class='navbar-brand' href='/newpost?User_id=" +
         userId +
@@ -21,6 +21,7 @@ $(document).ready(() => {
     );
   });
 
+  // get all posts in database by user to be displayed in user post section
   function getUserPosts() {
     $.get("/api/posts/user", data => {
       posts = data;
@@ -32,7 +33,51 @@ $(document).ready(() => {
     });
   }
 
+  // calls above function
+  getUserPosts();
+
+  // InitializeGlobalRows handles appending all of our constructed post HTML inside
+  function initializeUserRows() {
+    userContainer.empty();
+    const postsToAdd = [];
+    for (let i = 0; i < posts.length; i++) {
+      postsToAdd.push(createNewUserRow(posts[i]));
+    }
+    userContainer.append(postsToAdd);
+  }
+
+  // format for creating cards html for the user post section based off posts with user id
+  function createNewUserRow(post) {
+    const newPostCard = $(`<div class="mb-5 card" style="border-radius: 2em">
+    <div class="card-header" style="border-top-right-radius: 2em; border-top-left-radius: 2em">
+    <h4 class="card-title">${post.title}</h4>
+    <h6 class="card-location">${post.location}</h6>
+    <h6 class="card-category">${post.category}</h6>
+   <button type="button" value="${post.id}" class="btn btn-primary edit">Edit</button>
+    <button type="button" value="${post.id}" class="btn btn-danger delete" >Delete</button>
+    </div>
+    <div class="card-body">
+      <p class="card-text">${post.body}</p>
+      <img src="${post.image}" alt="" style="width:200px; height:200px">
+      <br />
+      <a href="mailto:${post.email}">${post.email}</a>
+    </div>
+  </div>`);
+    return newPostCard;
+  }
+
+  // if there are no posts in comunity post section display this
+  function displayEmpty() {
+    golbalContainer.empty();
+    const messageH2 = $(
+      `<h2 style="text-align:center">No Community Posts</h2>`
+    );
+    golbalContainer.append(messageH2);
+  }
+
+  // get all posts in database to be displayed in community post section
   function getGlobalPosts(category) {
+    // enables post to be rendered by catagorey if the catagory drop down is changed
     let categoryString = category || "";
     if (categoryString) {
       categoryString = "/category/" + categoryString;
@@ -43,29 +88,28 @@ $(document).ready(() => {
         displayEmpty();
       } else {
         initializeGlobalRows(posts);
-        // each time a user changes a category refresh the location dropdown to only include the location in the category
         renderLocations(posts);
       }
     });
   }
 
-  // Getting the initial list of global posts
-  getUserPosts();
-  // InitializeGlobalRows handles appending all of our constructed post HTML inside
-  // blogContainer
-  function initializeUserRows() {
-    userContainer.empty();
-    const postsToAdd = [];
-    for (let i = 0; i < posts.length; i++) {
-      postsToAdd.push(createNewUserRow(posts[i]));
-    }
-    userContainer.append(postsToAdd);
+  // calls the above function
+  getGlobalPosts();
+
+  // looks at current list of locations in category and creates a dropdown menu to filter by location
+  function renderLocations(filteredPosts) {
+    locations.empty();
+    locations.append(
+      `<option selected value="All Locations">All Locations</option>`
+    );
+    const cities = filteredPosts.map(filteredPosts => filteredPosts.location);
+    const noDuplicateCity = Array.from(new Set(cities));
+    noDuplicateCity.forEach(city => {
+      locations.append(`<option value="${city}">${city}</option>`);
+    });
   }
 
-  // Getting the initial list of global posts
-  getGlobalPosts();
   // InitializeGlobalRows handles appending all of our constructed post HTML inside
-  // blogContainer
   function initializeGlobalRows(filteredPosts) {
     golbalContainer.empty();
     const postsToAdd = [];
@@ -74,7 +118,8 @@ $(document).ready(() => {
     }
     golbalContainer.append(postsToAdd);
   }
-  // This function constructs a post's HTML
+
+  // format for creating cards html for the comunity post section
   function createNewRow(post) {
     const newPostCard = $(`<div class="mb-5 card" style="border-radius: 2em">
     <div class="card-header" style="border-top-right-radius: 2em; border-top-left-radius: 2em">
@@ -92,25 +137,14 @@ $(document).ready(() => {
     return newPostCard;
   }
 
-  function createNewUserRow(post) {
-    const newPostCard = $(`<div class="mb-5 card" style="border-radius: 2em">
-      <div class="card-header" style="border-top-right-radius: 2em; border-top-left-radius: 2em">
-      <h4 class="card-title">${post.title}</h4>
-      <h6 class="card-location">${post.location}</h6>
-      <h6 class="card-category">${post.category}</h6>
-     <button type="button" value="${post.id}" class="btn btn-primary edit">Edit</button>
-      <button type="button" value="${post.id}" class="btn btn-danger delete" >Delete</button>
-      </div>
-      <div class="card-body">
-        <p class="card-text">${post.body}</p>
-        <img src="${post.image}" alt="" style="width:200px; height:200px">
-        <br />
-        <a href="mailto:${post.email}">${post.email}</a>
-      </div>
-    </div>`);
-    return newPostCard;
+  // if there are no posts in comunity post section display this
+  function userDisplayEmpty() {
+    userContainer.empty();
+    const messageH2 = $(`<h2 style="text-align:center">No User Posts</h2>`);
+    userContainer.append(messageH2);
   }
 
+  // specifing a post id to delete by when the delete button is clicked
   function handlePostDelete() {
     const id = $(this).val();
     $.ajax({
@@ -125,31 +159,19 @@ $(document).ready(() => {
       });
   }
 
+  // directing the edit button to the newpost page and including the post id for reference in the html
   function handlePostEdit() {
     const id = $(this).val();
     window.location.href = "/newpost?post_id=" + id;
   }
 
-  function userDisplayEmpty() {
-    userContainer.empty();
-    // eslint-disable-next-line quotes
-    const messageH2 = $(`<h2 style="text-align:center">No User Posts</h2>`);
-    userContainer.append(messageH2);
-  }
-  function displayEmpty() {
-    golbalContainer.empty();
-    const messageH2 = $(
-      // eslint-disable-next-line quotes
-      `<h2 style="text-align:center">No Community Posts</h2>`
-    );
-    golbalContainer.append(messageH2);
-  }
   // This function handles reloading new posts when the category changes
   function handleCategoryChange() {
     const newPostCategory = $(this).val();
     getGlobalPosts(newPostCategory);
   }
 
+  // function called when location drop down is changed
   function handleLocationCahange() {
     newLocation = $(this).val();
     if (newLocation === "All Locations") {
@@ -160,20 +182,9 @@ $(document).ready(() => {
     }
   }
 
+  // event handelers for members page
   $(document).on("click", "button.delete", handlePostDelete);
   $(document).on("click", "button.edit", handlePostEdit);
   postCategory.on("change", handleCategoryChange);
   locations.on("change", handleLocationCahange);
-
-  function renderLocations(filteredPosts) {
-    locations.empty();
-    locations.append(
-      `<option selected value="All Locations">All Locations</option>`
-    );
-    const cities = filteredPosts.map(filteredPosts => filteredPosts.location);
-    const noDuplicateCity = Array.from(new Set(cities));
-    noDuplicateCity.forEach(city => {
-      locations.append(`<option value="${city}">${city}</option>`);
-    });
-  }
 });
